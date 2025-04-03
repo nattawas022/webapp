771ada69062cfbb3c0e9a923c6cc5a808186cec5
Nattawas Sawamgrat <nattawas488@gmail.com>
2025-04-03 11:25:45 +0700

    hi

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'computing.psu.ac.th',
-        pathname: '/**',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
